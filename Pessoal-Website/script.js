5196bd2eb18c7beb3534e80bcee595cc126009ee
Davi Teixeira <davideazevedo72@gmail.com>
2024-06-27 21:49:10 -0300

    Add files via upload

---
+++ Pessoal-Website/script.js
@@ -0,0 +1,6 @@
+/*PreLoader funciton*/
+function preLoaderJS() {
+    document.getElementsByClassName("PreLoaderContent")[0].style.display = "none";
+    document.getElementById("content").style.display = "block";
+}
+
